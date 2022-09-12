4fbfa8096531f47099d918de46eb6edd1e025b4b
Odia <94786718+AustinAdodo@users.noreply.github.com>
2022-09-12 19:04:06 +0100

    Add files via upload

---
+++ Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Grpc.Net.Client;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System.Collections.Generic;
+using DotNETgRPCServiceInteractionDemo;
+
+            var responseClient = new MaintenanceLogService.MaintenanceLogServiceClient(GrpcChannel.ForAddress("http://localhost:5000"));  ///server address
+            List<LogParam> Logs = new();
+            Logs.Add(new LogParam
+            {
+                EngrFullName = "Henrich Larsson",
+                Date = DateTime.Today.ToString(),
+                Id = 2,
+                IsCompleted = true,
+                Item = "Piston Crank",
+                ItemId = 100002
+            });
+            Logs.Add(new LogParam
+            {
+                EngrFullName = "Adodo Austin",
+                Date = DateTime.Today.ToString(),
+                Id = 3,
+                IsCompleted = true,
+                Item = "Air filter",
+                ItemId = 100003
+            });
+            Logs.Add(new LogParam
+            {
+                EngrFullName = "Ian Flemming",
+                Date = DateTime.Today.ToString(),
+                Id = 4,
+                IsCompleted = true,
+                Item = "Central Bushin Actuator",
+                ItemId = 100004
+            });
+            //create async
+            var stream = await responseClient.CreateLogAsync(new LogParam
+            {
+                EngrFullName = "Mattias Sammer",
+                Date = DateTime.Today.ToString(),
+                Id = 1,
+                IsCompleted = true,
+                Item = "HVAC Central Valve",
+                ItemId = 100001
+            });
+            var empty = new Empty();
+            Console.WriteLine((stream.Id == 1) ? "StartRequest...." + stream : stream);
+            foreach (LogParam log in Logs)
+            {
+                var stream2 = await responseClient.CreateLogAsync(log);
+                Console.WriteLine("Chunk,  Payload......"+ stream2.Id + stream2 + "\n");
+            }
---
+++ DotNETgRPCServiceInteractionDemo/Services/LoggingService.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace DotNETgRPCServiceInteractionDemo.Services
+{
+    public class loggingservice : MaintenanceLogService.MaintenanceLogServiceBase
+    {
+        private readonly ILogger<loggingservice> _logger;
+        public loggingservice(ILogger<loggingservice> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<LogParam> CreateLog(LogParam initlog, ServerCallContext context)
+        {
+            return Task.FromResult(new LogParam
+            {
+                EngrFullName = initlog.EngrFullName,
+                Date = initlog.Date,
+                IsCompleted = true,
+                Id = initlog.Id,
+                Item = initlog.Item,
+                ItemId = initlog.ItemId,
+                Status = "Successfully sent to server "
+            }) ;
+        }
+
+        public override Task<LogParams> RetrieveLogs(Empty request,ServerCallContext context)
+        {
+            return Task.FromResult(new LogParams
+            { 
+                Status = "Successfully retrieved from server ",
+            });
+        }
+    }
+}
